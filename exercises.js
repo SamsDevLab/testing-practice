bd4c55caa3c6070b21fb6994449f822c63806ede
Sam Hadfield <sthtech@protonmail.com>
2025-10-03 11:27:53 -0500

    feat: create capitalize and reverseStr fns
    Create capitalize and reverseStr functions for testing. Export both
    functions

--- exercises.js
+++ exercises.js
@@ -1,1 +1,13 @@
-export function capitalize(str) {}
+export function capitalize(str) {
+  const firstLetter = str.charAt(0).toUpperCase();
+  const slicedStr = str.slice(1);
+  const newStr = firstLetter.concat(slicedStr);
+
+  return newStr;
+}
+
+export function reverseString(str) {
+  const reversedStr = str.split("").reverse().join("");
+
+  return reversedStr;
+}
