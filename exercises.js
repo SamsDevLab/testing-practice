828f7797fe483414a3594c753e104e713f0bc571
Sam Hadfield <sthtech@protonmail.com>
2025-10-07 15:42:49 -0500

    fix: refactor caesarCipher with modulo

--- exercises.js
+++ exercises.js
@@ -31,114 +31,39 @@ export const calculator = {
   },
 };
 
-/*
-Pseudo:
-
-• Use ASCII values with charCode 97 - 122 (a-z)
-• Need to iterate through each char of the string and add new cipher letter to an array
-    • With the exception of punctuation - just add this to the array (.,!) etc.
-    • Also, spaces are an exception but they need to be included in the array as well
-*/
-
-const shiftLowerCase = (charCode, shiftFactor) => {
-  const lowerCaseA = "a";
-  const lowerCaseZ = "z";
-
-  const lowerCaseACode = lowerCaseA.charCodeAt(0);
-  const lowerCaseZCode = lowerCaseZ.charCodeAt(0);
-
-  const shiftedCharCode = charCode + shiftFactor;
-
-  if (shiftedCharCode < lowerCaseACode) {
-    const subtractedValue = lowerCaseACode - shiftedCharCode;
-    const newLetter = String.fromCharCode(lowerCaseZCode + 1 - subtractedValue);
-    return newLetter;
-  } else if (shiftedCharCode > lowerCaseZCode) {
-    const subtractedValue = shiftedCharCode - lowerCaseZCode;
-    const newLetter = String.fromCharCode(lowerCaseACode - 1 + subtractedValue);
-    return newLetter;
-  } else {
-    const newLetter = String.fromCharCode(shiftedCharCode);
-    return newLetter;
-  }
+const getBaseNum = (charCode) => {
+  if (charCode >= 97 && charCode <= 122) {
+    return 97;
+  } else if (charCode >= 65 && charCode <= 90) {
+    return 65;
+  } else return null;
 };
 
-const shiftUpperCase = (charCode, shiftFactor) => {
-  const upperCaseA = "A";
-  const upperCaseZ = "Z";
+const shiftNum = (charCode, shiftFactor, baseNum) => {
+  const modulus = 26;
 
-  const upperCaseACode = upperCaseA.charCodeAt(0);
-  const upperCaseZCode = upperCaseZ.charCodeAt(0);
+  const workingNum = charCode - baseNum;
 
-  const shiftedCharCode = charCode + shiftFactor;
+  const shiftedNum =
+    ((((workingNum + shiftFactor) % modulus) + modulus) % modulus) + baseNum;
 
-  if (shiftedCharCode < upperCaseACode) {
-    const subtractedValue = upperCaseACode - shiftedCharCode;
-    const newLetter = String.fromCharCode(upperCaseZCode + 1 - subtractedValue);
-    return newLetter;
-  } else if (shiftedCharCode > upperCaseZCode) {
-    const subtractedValue = shiftedCharCode - upperCaseZCode;
-    const newLetter = String.fromCharCode(upperCaseACode - 1 + subtractedValue);
-    return newLetter;
-  } else {
-    const newLetter = String.fromCharCode(shiftedCharCode);
-    return newLetter;
-  }
+  const newLetter = String.fromCharCode(shiftedNum);
+
+  return newLetter;
 };
 
 export function caesarCipher(str, shiftFactor) {
   const newLetterArr = [];
 
   for (const char of str) {
-    const charCode = char.charCodeAt(char); // 120, 121, 122 (final codes in alphabet)
+    const charCode = char.charCodeAt(char);
 
-    if (charCode >= 97 && charCode <= 122) {
-      const newLetter = shiftLowerCase(charCode, shiftFactor);
-      newLetterArr.push(newLetter);
-    } else if (charCode >= 65 && charCode <= 90) {
-      const newLetter = shiftUpperCase(charCode, shiftFactor);
-      newLetterArr.push(newLetter);
-    } else {
+    const baseNum = getBaseNum(charCode);
+
+    if (baseNum === null) {
       newLetterArr.push(char);
-    }
-  }
-
-  console.log(newLetterArr);
-  const newStr = newLetterArr.join("");
-
-  return newStr;
-}
-
-/* 
-First Draft: 
-
-export function caesarCipher(str, shiftFactor) {
-  const newLetterArr = [];
-
-  const lowerCaseA = "a";
-  const lowerCaseZ = "z";
-
-  const lowerCaseACode = lowerCaseA.charCodeAt(0);
-  const lowerCaseZCode = lowerCaseZ.charCodeAt(0);
-
-  for (const char of str) {
-    const charCode = char.charCodeAt(char); // 120, 121, 122 (final codes in alphabet)
-    const shiftedCharCode = charCode + shiftFactor; // 123, 124, 125 (wrap to front (97,98, 99))
-
-    if (shiftedCharCode < lowerCaseACode) {
-      const subtractedValue = lowerCaseACode - shiftedCharCode;
-      const newLetter = String.fromCharCode(
-        lowerCaseZCode + 1 - subtractedValue
-      );
-      newLetterArr.push(newLetter);
-    } else if (shiftedCharCode > lowerCaseZCode) {
-      const subtractedValue = shiftedCharCode - lowerCaseZCode;
-      const newLetter = String.fromCharCode(
-        lowerCaseACode - 1 + subtractedValue
-      );
-      newLetterArr.push(newLetter);
     } else {
-      const newLetter = String.fromCharCode(shiftedCharCode);
+      const newLetter = shiftNum(charCode, shiftFactor, baseNum);
       newLetterArr.push(newLetter);
     }
   }
@@ -147,4 +72,3 @@ export function caesarCipher(str, shiftFactor) {
 
   return newStr;
 }
-*/
