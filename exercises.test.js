f475a4b4cb17601dc0fa16c369c1ba4fe53f3478
Sam Hadfield <sthtech@protonmail.com>
2025-10-07 17:27:06 -0500

    fix: remove unnecessary test for analyzeArray

--- exercises.test.js
+++ exercises.test.js
@@ -79,27 +79,6 @@ test("takes a string with punctuation and a shift factor, returns shifted string
   expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
 
-// test("takes an array of numbers and returns an object with the following properties: average", () => {
-//   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
-//     average: 4,
-//   });
-// });
-
-// test("takes an array of numbers and returns an object with the following properties: average and min", () => {
-//   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
-//     average: 4,
-//     min: 1,
-//   });
-// });
-
-// test("takes an array of numbers and returns an object with the following properties: average,min, and max", () => {
-//   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
-//     average: 4,
-//     min: 1,
-//     max: 8,
-//   });
-// });
-
 test("takes an array of numbers and returns an object with the following properties: average, min, max, and length", () => {
   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
